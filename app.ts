b55bb754c4a900a41ca13bdb5fabc18fd87cc8b6
WILLIAM LUCIO DO NASCIMENTO <williamluciodonascimento@MacBook-Pro-de-WILLIAM.local>
2018-07-12 00:26:50 -0400

    aula typescript sobre tipos de variaveis

---
+++ app.ts
@@ -0,0 +1,13 @@
+let message: string = "Ola, estamos testando o app typescript!"
+console.log(message);
+
+let episode: number = 4
+console.log("This is episode" + 4)
+episode = episode + 1
+console.log("Next episode is" + episode)
+
+// tipo Any não é nescessario declarar o tipo
+let favoritDroid //let só é acessada dentro do bloco
+favoritDroid = 'BB-8'
+// favoritDroid = 10
+console.log("My favorite droid is "+favoritDroid)
---
+++ dist/app.js
@@ -0,0 +1,11 @@
+var message = "Ola, estamos testando o app typescript!";
+console.log(message);
+var episode = 4;
+console.log("This is episode" + 4);
+episode = episode + 1;
+console.log("Next episode is" + episode);
+// tipo Any não é nescessario declarar o tipo
+var favoritDroid; //let só é acessada dentro do bloco
+favoritDroid = 'BB-8';
+// favoritDroid = 10
+console.log("My favorite droid is " + favoritDroid);
